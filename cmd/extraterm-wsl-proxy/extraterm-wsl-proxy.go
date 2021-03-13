0584ebc26623b4cb12f67b282f5c4fc2536c69c4
Simon Edwards <simon@simonzone.com>
2021-03-13 10:22:50 +0100

    Fix up copyright headers

--- cmd/extraterm-wsl-proxy/extraterm-wsl-proxy.go
+++ cmd/extraterm-wsl-proxy/extraterm-wsl-proxy.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 Simon Edwards <simon@simonzone.com>
+ *
+ * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
+ */
 package main
 
 import (
